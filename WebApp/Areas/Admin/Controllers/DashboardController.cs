469799c18eeec672dacea5db07fc8f95297bd040
kamal24h <kbastani@gmail.com>
2026-01-03 22:26:21 +0330

    ++

--- WebApp/Areas/Admin/Controllers/DashboardController.cs
+++ WebApp/Areas/Admin/Controllers/DashboardController.cs
@@ -11,12 +11,12 @@ namespace WebApp.Areas.Admin.Controllers
     {
         public IActionResult Index()
         {
-            var culture = Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName;
+            //var culture = Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName;
 
-            if (culture == "fa")
-                ViewBag.AdminLayout = "_AdminLayout.fa.cshtml";
-            else
-                ViewBag.AdminLayout = "_AdminLayout.en.cshtml";
+            //if (culture == "fa")
+            //    ViewBag.AdminLayout = "_AdminLayout.fa.cshtml";
+            //else
+            //    ViewBag.AdminLayout = "_AdminLayout.en.cshtml";
             return View();
         }
 
